7ed1c923c76b40ba85184c801837f41a0d7f3302
MateusSJames <mateusaristides23@gmail.com>
2023-04-26 16:58:56 -0300

    feat: add alert for show status login

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -6,10 +6,19 @@ button?.addEventListener('click', async (e) => {
     const loginService = new LoginService();
     const email = (document.getElementById('email') as HTMLInputElement)?.value
     const senha = (document.getElementById('senha') as HTMLInputElement)?.value
+    const alert = document.getElementById('alert-content')
     const credentials = {
         email: email,
         senha: senha
     }
     const responseLogin = await loginService.sendLogin(credentials);
-    console.log(responseLogin)
+    if(responseLogin.status != 200) {
+        if(alert != null) {
+            alert.style.visibility = "visible";
+            setTimeout(() => {
+                alert.style.visibility = "hidden";
+            }, 3000);
+            
+        }
+    }
 })
