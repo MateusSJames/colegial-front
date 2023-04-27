5415957d39c1b4a0554ad05a9c7c2faefb85e219
MateusSJames <mateusaristides23@gmail.com>
2023-04-27 09:29:42 -0300

    feat: add message error responsive in login screen

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -6,17 +6,25 @@ button?.addEventListener('click', async (e) => {
     const loginService = new LoginService();
     const email = (document.getElementById('email') as HTMLInputElement)?.value
     const senha = (document.getElementById('senha') as HTMLInputElement)?.value
-    const alert = document.getElementById('alert-content')
+    const alertContent = document.getElementById('alert-content')
     const credentials = {
         email: email,
         senha: senha
     }
     const responseLogin = await loginService.sendLogin(credentials);
+    const responseStatus: { [key: number]: string } = {
+        404: 'Dados incorretos, verifique e tente novamente',
+        401: 'Você não está liberado para usar esse sistema',
+        500: 'Erro interno no Servidor'
+    }
     if(responseLogin.status != 200) {
-        if(alert != null) {
-            alert.style.visibility = "visible";
+        if(alertContent != null) {
+            alertContent.style.visibility = "visible";
+            alertContent.innerHTML = `
+                    <h3>${responseStatus[responseLogin.status]}</h3>
+                `
             setTimeout(() => {
-                alert.style.visibility = "hidden";
+                alertContent.style.visibility = "hidden";
             }, 3000);
             
         }
