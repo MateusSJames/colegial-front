0f7dc941e457fc1f33533ebbe084adbda10f3a08
MateusSJames <mateusaristides23@gmail.com>
2023-04-28 11:54:02 -0300

    feat: add mock dates on buttons

--- src/scripts/home.ts
+++ src/scripts/home.ts
@@ -1,6 +1,27 @@
-const button = document.getElementById('parametros')
+const buttonParam = document.getElementById('parametros')
+const buttonPedido = document.getElementById('pedidos')
 
-button?.addEventListener('click', (event) => {
+buttonParam?.addEventListener('click', (event) => {
     event.preventDefault();
-    alert('Hello World');
+    alert('Hello Parametros');
+    const filterContent = document.getElementById('filters');
+    if(filterContent != null) {
+        filterContent.innerHTML = `
+            <h2>Status</h2>
+        `;
+    }
+})
+
+buttonPedido?.addEventListener('click', (event) => {
+    event.preventDefault();
+    alert('Hello Parametros');
+    const filterContent = document.getElementById('filters');
+    if(filterContent != null) {
+        filterContent.innerHTML = `
+            <h2>Solicitado</h2>
+            <h2>Separado</h2>
+            <h2>Enviado</h2>
+            <h2>Entregue</h2>
+        `;
+    }
 })
